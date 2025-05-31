96879b57fef037ae6de415a992f43b283a928dee
GungdeAngga <agungangga2001@gmail.com>
2025-05-31 20:13:08 +0800

    adjust benefit

--- components/footer/benefit.vue
+++ components/footer/benefit.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="flex flex-col">
         <span class="font-bold text-xl pb-2">E-commerce benefit</span>
-        <div v-for="benefit in benefits" :key="benefit.id" class="flex space-x-3 space-y-1">
-            <img :src="benefit.icon" class="w-6 h-6 invert"/>
+        <div v-for="benefit in benefits" :key="benefit.id" class="flex items-center space-x-3 space-y-1">
+            <img :src="benefit.icon" class="w-5 h-5 invert"/>
             <span>{{ benefit.title }}</span>
         </div>
     </div>
