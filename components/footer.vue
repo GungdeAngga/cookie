54c1ed9cd5c5ae1589c55a807d303e1833ca4695
GungdeAngga <agungangga2001@gmail.com>
2025-05-31 20:12:07 +0800

    adjust footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-slate-800 text-white min-h-64 w-full">
     <div class="max-w-[1200px] px-4 xl:px-0 m-auto">
-      <div class="grid grid-cols-2 py-3 border-b-2 border-slate-300">
+      <div class="grid grid-cols-1 space-y-2 md:space-y-0 md:grid-cols-2 py-3 border-b-2 border-slate-300">
         <FooterAbout />
         <FooterBenefit />
       </div>
-      <div class="py-3 px-4 xl:px-0">
+      <div class="py-3">
         © 2025 ShopMart E-commerce Platform. All rights reserved.
       </div>
     </div>
